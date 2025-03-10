e6ca8e7fa8940e6b4dc156fae4ffc10b441beff5
Ana Carolina <carol00_10@hotmail.com>
2025-03-10 01:45:32 -0300

    create translate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useThemeContext } from "../context/ThemeContext";
 import { useTranslation } from "react-i18next";
-import i18n from "i18next";
+import i18n from "../i18n";
 import logo from "../assets/logo.svg";
 
 
